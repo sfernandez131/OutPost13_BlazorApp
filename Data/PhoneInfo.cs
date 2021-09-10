25e74374f6b9828dfcd66778093e72dbc9e8fc2a
Scott Fernandez <sfernandez@goldstarfinancial.com>
2021-09-10 13:02:40 -0400

    phone lookup

---
+++ Data/PhoneInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace OutPost13.Data
+{
+    public class PhoneInfo
+    {
+        public Provider Providerr { get; set; }
+        public string SMSAddress { get; set; }
+        public string MMSAddress { get; set; }
+        public string DateFirstSeen { get; set; }
+        public string DateOfPorting { get; set; }
+        public string NoteCodes { get; set; }
+        public string NoteDescriptions { get; set; }
+        public string TokensUsed { get; set; }
+
+        public class Provider
+        {
+            public string Name { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Latitude { get; set; }
+            public string Longitude { get; set; }
+            public string LineType { get; set; }
+        }
+    }
+}
